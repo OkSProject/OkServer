be2fa05e16e3a2db551467d235a99563ab72c5cd
Jackson <realassdude@national.shitposting.agency>
2024-02-07 20:41:45 -0700

    Cleanup server

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -2,7 +2,6 @@ package server
 
 import (
 	"example.com/hello/pkg/config"
-	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -10,18 +9,11 @@ import (
 )
 
 func Run() {
-
-	// Creates /log directory to store server logs.
-	// Exits if an error occurs with the directory creation.
 	logDir := config.GetLogDir()
-	err := os.MkdirAll(logDir, os.ModePerm)
-	if err != nil {
+	if err := os.MkdirAll(logDir, os.ModePerm); err != nil {
 		log.Fatalf("Error creating log directory %s: %e", logDir, err)
 	}
 
-	// Creates or opens `server.log` for writing purposes.
-	// Exits if an error occurs while opening the log file.
-	// Closes the log file once done and sets log_file as the output.
 	logFile, err := os.OpenFile(logDir+"okserver.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		log.Fatal("Error opening log file: ", err)
@@ -29,23 +21,18 @@ func Run() {
 	defer logFile.Close()
 
 	// Allows for both writing a log file and printing it to the command line.
-	mwLog := io.MultiWriter(logFile, os.Stdout)
-	log.SetOutput(mwLog)
+	log.SetOutput(io.MultiWriter(logFile, os.Stdout))
 
 	// Serves site(s) from directory "/".
-	dir := config.GetAssetDir()
-	fileServer := http.FileServer(http.Dir(dir))
+	fileServer := http.FileServer(http.Dir(config.GetAssetDir()))
 	http.Handle("/", fileServer)
 
-	// Sets server to listen on Port 8080.
 	port := config.GetListenPort()
-
-	// Starts server.
-	log.Printf("OkServer! Running on: %s...\n", port)
+	log.Printf("OkServer! Running on :%s...\n", port)
 
 	// Error checking
 	// Sets "err" to the output of the HTTP server when it starts.
-	if err := http.ListenAndServe(fmt.Sprintf(":%s", port), nil); err != nil {
-		log.Println("Error: ", err)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal("Error: ", err)
 	}
 }
